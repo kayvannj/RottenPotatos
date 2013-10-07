4cc71cd2d75df57e01648df164ff28863ea57931
kayvannj <kayvan.najafzadeh@gmail.com>
2013-10-06 17:18:41 -0700

    CRUD all implemented

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,3 +1,10 @@
 class Movie < ActiveRecord::Base
+
+	@@all_ratings = ['G','PG','PG-13','R']
+	
+	def self.all_ratings
+		@@all_ratings
+	end
+	
 	attr_accessible :title, :rating, :description, :release_date
 end
